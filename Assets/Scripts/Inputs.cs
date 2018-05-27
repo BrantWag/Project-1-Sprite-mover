9e1541a2ce5f20f401eda9ba37bc1c7420e6819e
Brant Wagner <brawagne@uat.edu>
2018-05-27 16:47:40 -0700

    Add files via upload

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour {
+	public Transform tf;
+	private Movement movementDisable; // Get a reference to to my Movement and name it movementDisable
+
+
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform> (); //calls transfrom component
+		{
+			movementDisable = GetComponent<Movement>(); //  hold the Movement script component
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.Space)) {
+			tf.position = Vector3.zero; //returns sprite to center of the world 
+		}
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			gameObject.SetActive (false); //sets game object to off
+		} 
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();//cleanly extis the app 
+		}
+		if (Input.GetKeyDown (KeyCode.P)) // Check to see if P was pressed down & will toggle the movement for the shuttle on and off
+		{
+			movementDisable.enabled = !movementDisable.enabled; // Access script's enabled function and set it to On/Off
+		}
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public Transform tf;
+	public float speed;
+	private GameObject spaceship;
+	// Use this for initialization
+	void Start () 	{
+		tf = GetComponent<Transform> (); //calls transfrom component
+		spaceship = GetComponent<GameObject>();//sets game object to true 
+		//spaceship = GetComponent();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		tf.position = tf.position + Vector3.zero;// Puts sprite in center of world 
+		if (Input.GetKey (KeyCode.LeftShift) && Input.GetKey (KeyCode.RightArrow)) {
+			tf.position = tf.position + Vector3.right * .05f; //Moves Sprite one unit right
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			tf.position = tf.position + Vector3.right;//moves sprite right
+		}
+		if (Input.GetKey (KeyCode.LeftShift) && Input.GetKey (KeyCode.LeftArrow)) {
+			tf.position = tf.position + Vector3.left * .05f; //Moves Sprite one unit left
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			tf.position = tf.position + Vector3.left;//moves sprite left
+		}
+		if (Input.GetKey (KeyCode.LeftShift) && Input.GetKey (KeyCode.DownArrow)) {
+			tf.position = tf.position + Vector3.down * .05f; //Moves Sprite one unit down
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			tf.position = tf.position + Vector3.down;//moves sprite down
+		}
+		if (Input.GetKey (KeyCode.LeftShift) && Input.GetKey (KeyCode.UpArrow)) {
+			tf.position = tf.position + Vector3.up * .05f; //Moves Sprite one unit 
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			tf.position = tf.position + Vector3.up; //moves sprite up
+		}
+ 
+	}
+}
